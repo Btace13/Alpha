40cd60f7597526dfb37d0cf8ce17f9b85dd58a8d
Enrique Urbina <btace1347@yahoo.com>
2018-07-21 16:11:42 -0500

    working on converting to bootstrap

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,31 +1,28 @@
 <template>
 <div>
-    <nav id="navbar">
-        <!-- Dropdown Structure -->
-        <ul id="dropdown1" class="dropdown-content">
-            <li><router-link to="/profile">Profile</router-link></li>
-            <li class="divider"></li>
-            <li><router-link to="/logout">Log Out</router-link></li>
-        </ul>
-        <div class="nav-wrapper">
-            <router-link v-show="user" style="padding-left: 10px;" to="/feed" class="brand-logo">Project: Idea</router-link>
-            <router-link v-show="!user" style="padding-left: 10px;" to="/" class="brand-logo">Project: Idea</router-link>
-            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-                <li><router-link to="/about">About</router-link></li>
-                <li  v-show="user"><a class="dropdown-trigger" href="#!" data-target="dropdown1">{{user && user.displayName}}<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li v-show="!user"><router-link to="/login">Login</router-link></li>
-            </ul>
-        </div>
-    </nav>
+     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+       <router-link v-show="user" style="padding-left: 10px;" to="/feed" class="navbar-brand mr-auto mr-lg-0"> <img style="margin-left: 20px;" class="logo" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/626071/line-logo.svg"></router-link>
+       <router-link v-show="!user" style="padding-left: 10px;" to="/" class="navbar-brand mr-auto mr-lg-0"> <img style="margin-left: 20px;" class="logo" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/626071/line-logo.svg"></router-link>
+      <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <ul class="sidenav" id="mobile-demo">
-        <li><router-link to="/about">About</router-link></li>
-        <li v-show="user"><router-link to="/profile">Profile</router-link></li>
-        <li class="divider"></li>
-        <li v-show="user"><router-link to="/logout">Log Out</router-link></li>
-        <li v-show="!user"><router-link to="/login">Login</router-link></li>
-    </ul>
+      <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
 
 </div>
 </template>
@@ -40,11 +37,84 @@ export default {
 }
 </script>
 <style scoped>
-    #navbar{
-        background: #2d4059;
-    }
-    .dropdown-trigger{
-        color: #ffb400;
-    }
+    html,
+body {
+  overflow-x: hidden; /* Prevent scroll on narrow devices */
+}
+
+body {
+  padding-top: 56px;
+}
+
+@media (max-width: 991.98px) {
+  .offcanvas-collapse {
+    position: fixed;
+    top: 56px; /* Height of navbar */
+    bottom: 0;
+    left: 100%;
+    width: 100%;
+    padding-right: 1rem;
+    padding-left: 1rem;
+    overflow-y: auto;
+    visibility: hidden;
+    background-color: #343a40;
+    transition-timing-function: ease-in-out;
+    transition-duration: .3s;
+    transition-property: left, visibility;
+  }
+  .offcanvas-collapse.open {
+    left: 0;
+    visibility: visible;
+  }
+}
+
+.nav-scroller {
+  position: relative;
+  z-index: 2;
+  height: 2.75rem;
+  overflow-y: hidden;
+}
+
+.nav-scroller .nav {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  padding-bottom: 1rem;
+  margin-top: -1px;
+  overflow-x: auto;
+  color: rgba(255, 255, 255, .75);
+  text-align: center;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}
+
+.nav-underline .nav-link {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+  font-size: .875rem;
+  color: #6c757d;
+}
+
+.nav-underline .nav-link:hover {
+  color: #007bff;
+}
+
+.nav-underline .active {
+  font-weight: 500;
+  color: #343a40;
+}
+
+.text-white-50 { color: rgba(255, 255, 255, .5); }
+
+.bg-purple { background-color: #6f42c1; }
+
+.border-bottom { border-bottom: 1px solid #e5e5e5; }
+
+.box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); }
+
+.lh-100 { line-height: 1; }
+.lh-125 { line-height: 1.25; }
+.lh-150 { line-height: 1.5; }
 </style>
 
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="hello">
-   <h1>PROJECT: IDEA</h1>
-       <p>Here is where is ideas and creators  collide!</p>
-    <router-link to="/login">GET STARTED</router-link>
+<div>
+  <div class="wrapper">
+    <div class="header">
+      <div class="content">
+        <h1>Project: Idea</h1>
+        <h2>Where ideas and creators collide!</h2>
+        <div class="buttons">
+          <router-link class="button" to="/login">Get Started</router-link>
+        </div>
+      </div>
+      <img class="logo" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/626071/line-logo.svg">
+    </div>
   </div>
+  <div class="background"></div>
+</div>
 </template>
 
 <script>
@@ -19,31 +29,132 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1 {
-    font-size: 3em;
-    padding-top: 20%;
-    margin: 0;
+ .background {
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  background: #fc3a52;
+  background: -moz-linear-gradient(top, #F9B248 0%, #030408 100%);
+  background: -webkit-linear-gradient(top, #F9B248 0%, #030408 100%);
+  background: linear-gradient(to bottom, #F9B248 0%, #030408 100%);
 }
-  .hello{
-    height: 100vh;
-    width: 100%;
-    background: url('../assets/img/back3.jpg');
-    background-size: cover;
-    background-position: right;
-  }
-  p{
-    margin-bottom: 60px;
-  }
-  a{
-    background: #ea5455;
-    color: #f6f6f6;
-    margin-top: 20px;
-    padding: 15px 40px;
-    border-radius: 8px;
-  }
-  a:hover{
-    background: #2d4059;
-    color: #ffb400;
-    border: 1px solid #000;
-  }
+.wrapper {
+  width: 80%;
+  height: 100%;
+  min-height: 800px;
+  background: #fff;
+  position: relative;
+  left: 50%;
+  top: 0;
+  margin: 90px 0;
+  transform: translate(-50%, 0);
+  -webkit-transform: translate(-50%, 0);
+  -moz-transform: translate(-50%, 0);
+  -webkit-box-shadow: 0px 0px 30px 5px rgba(0,0,0,0.15);
+  -moz-box-shadow: 0px 0px 30px 5px rgba(0,0,0,0.15);
+  box-shadow: 0px 0px 30px 5px rgba(0,0,0,0.15);
+}
+.wrapper .header {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: #0E2431;
+  background: -moz-linear-gradient(top, #F9B248 0%, #FC3A52 100%);
+  background: -webkit-linear-gradient(top, #F9B248 0%, #FC3A52 100%);
+  background: linear-gradient(to bottom, #F9B248 0%, #FC3A52 100%);
+  overflow: hidden;
+}
+.wrapper .header .logo {
+  width: 50px;
+  height: auto;
+  position: absolute;
+  top: 50px;
+  left: 100px;
+}
+.wrapper .header .content {
+  position: absolute;
+  width: 100%;
+  height: auto;
+  max-width: 800px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -85%);
+}
+.wrapper .header .content h2{
+  color: #fff;
+  margin: 0;
+  font-size: 2em;
+  line-height: 1.2em;
+  width: 100%;
+  text-align: center;
+}
+.wrapper .header .content h1 {
+  color: #0E2431;
+  margin: 0;
+  font-size: 3.4em;
+  line-height: 2em;
+  width: 100%;
+  text-align: center;
+}
+.wrapper .header .content .buttons {
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+.wrapper .header .content .button {
+  display: inline-block;
+  width: 150px;
+  height: 50px;
+  background: #000;
+  color: #fff;
+  font-size: 1.2em;
+  text-decoration: none;
+  text-align: center;
+  line-height: 50px;
+  margin: 40px 5px 0;
+  -moz-border-radius: 5px;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  -webkit-box-shadow: 0px 12px 20px -10px rgba(0,0,0,0.65);
+  -moz-box-shadow: 0px 12px 20px -10px rgba(0,0,0,0.65);
+  box-shadow: 0px 12px 20px -10px rgba(0,0,0,0.65);
+  transition: box-shadow 0.5s;
+}
+.wrapper .header .content .button:hover {
+  -webkit-box-shadow: 0px 0px 20px -10px rgba(0,0,0,0.65);
+  -moz-box-shadow: 0px 0px 20px -10px rgba(0,0,0,0.65);
+  box-shadow: 0px 0px 20px -10px rgba(0,0,0,0.65);
+  transition: box-shadow 0.5s;
+}
+.wrapper .header .waves {
+  width: calc(100% + 10px);
+  height: auto;
+  position: absolute;
+  bottom: -3px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  -webkit-transform: translate(-50%, 0);
+  -moz-transform: translate(-50%, 0);
+  z-index: 0;
+}
+.wrapper .header .curve {
+  display: none;
+  width: calc(100% + 10px);
+  height: auto;
+  position: absolute;
+  bottom: -1px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  -webkit-transform: translate(-50%, 0);
+  -moz-transform: translate(-50%, 0);
+  z-index: 1;
+}
+
 </style>
